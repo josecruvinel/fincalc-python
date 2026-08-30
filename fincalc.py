4f12cf15b20068dbb4bcac385ae45a423accd3c9
josecruvinel <127355441+josecruvinel@users.noreply.github.com>
2026-08-30 23:31:21 +0000

    feat: adiciona funcao de juros compostos

--- fincalc.py
+++ fincalc.py
@@ -5,7 +5,14 @@ def calcular_juros_simples(capital: float, taxa_anual: float, anos: int) -> floa
     juros = capital * (taxa_anual / 100) * anos
     return capital + juros
 
+def calcular_juros_compostos(capital: float, taxa_anual: float, anos: int) -> float:
+    """Calcula o montante final obtido por juros compostos."""
+    montante = capital * ((1 + (taxa_anual / 100)) ** anos)
+    return montante
+
 if __name__ == "__main__":
     print("Iniciando o sistema FinCalc...")
     montante = calcular_juros_simples(1000.0, 5.0, 2)
-    print(f"Juros Simples (R$ 1.000 a 5% por 2 anos): R$ {montante:.2f}")
+    print(f"Juros Simples: R$ {montante:.2f}")
+    montante_comp = calcular_juros_compostos(1000.0, 5.0, 2)
+    print(f"Juros Compostos: R$ {montante_comp:.2f}")
